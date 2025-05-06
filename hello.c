f3f76e5d493083c5b674ed409d8ac1b0b9cd831b
Susan Mengel <susan.mengel@ttu.edu>
2025-05-06 07:46:31 -0500

    Adding TTU

--- hello.c
+++ hello.c
@@ -4,5 +4,6 @@ int main (void) {
     printf ("Hello World and Sky\n");
     printf ("Hello CS 2413\n");
     printf ("Hello Computer Science Department\n");
+    printf ("Hello Texas Tech University\n");
     return (0);
 }
